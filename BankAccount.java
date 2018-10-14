b67ec7e08d7c6aad7861279d130a108d4be4f0b8
grace-mao23 <gmao00@stuy.edu>
2018-10-14 16:06:20 -0400

    Forgot to commit; contains full code

---
+++ BankAccount.java
@@ -0,0 +1,51 @@
+public class BankAccount{
+  // instance variables
+  private double balance;
+  private int accountID;
+  private String password;
+
+  // constructor
+  public BankAccount(double newBalance, int newAccountID, String newPassword){
+    balance = newBalance;
+    accountID = newAccountID;
+    password = newPassword;
+  }
+
+  // get methods (accessor)
+  public double getBalance(){
+    return balance;
+  }
+  public int getAccountID(){
+    return accountID;
+  }
+
+  // set method
+  public void setPassword(String newPassword){
+    password = newPassword;
+  }
+
+  // toString
+  public String toString(){
+    return accountID + "\t" + balance;
+  }
+
+  // mutator methods
+  public boolean deposit(double money){
+    if (money < 0.0) {
+      return false;
+    } else {
+      balance += money;
+      return true;
+    }
+  }
+
+  public boolean withdraw(double money){
+    if (balance < money) {
+      return false;
+    } else {
+      balance -= money;
+      return true;
+    }
+  }
+
+}
