d5ff02e45cb98ebc7ce2357eaf76840ad518ba56
grace-mao23 <gmao00@stuy.edu>
2018-10-14 16:19:14 -0400

    Added transferTo

--- BankAccount.java
+++ BankAccount.java
@@ -49,6 +49,28 @@ public class BankAccount{
   }
 
   private boolean authenticate(String password){
-    return this.password.equals();
+    return this.password.equals(password);
   }
+
+
+  /**Transfer money from this BankAccount to the other only when the password matches, and the withdrawal succeeds.
+
+ *@param other which BankAccount to GIVE the money to
+
+ *@param amount how much money to transfer, negative amounts will make this operation fail.
+
+ *@param password to be checked against the source account, incorrect password will make this operation fail.
+
+ *@return true if successful, false otherwise.
+
+ */
+
+ public boolean transferTo(BankAccount other, double amount, String password){
+   if (!this.authenticate(password) || amount < 0 || amount > balance) {
+     return false;
+   }
+   balance -= amount;
+   other.balance += amount;
+   return true;
+ }
 }
